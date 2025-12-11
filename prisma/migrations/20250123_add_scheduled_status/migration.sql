e6f5c477a297dbc7602aaa8e2793f4f0f2e0ad66
Hustl-JBR <team.hustlapp@outlook.com>
2025-12-11 14:48:22 -0500

    Fix migration: Use simple ALTER TYPE syntax like previous migrations

--- prisma/migrations/20250123_add_scheduled_status/migration.sql
+++ prisma/migrations/20250123_add_scheduled_status/migration.sql
@@ -1,18 +1,4 @@
--- Add SCHEDULED status to JobStatus enum
-DO $$
-BEGIN
-    -- Check if SCHEDULED already exists before adding
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_enum 
-        WHERE enumlabel = 'SCHEDULED' 
-        AND enumtypid = (SELECT oid FROM pg_type WHERE typname = 'jobstatus')
-    ) THEN
-        ALTER TYPE "JobStatus" ADD VALUE 'SCHEDULED';
-    END IF;
-EXCEPTION
-    WHEN duplicate_object THEN
-        -- Enum value already exists, ignore
-        NULL;
-END
-$$;
+-- AlterEnum
+-- Add SCHEDULED to JobStatus enum
+ALTER TYPE "JobStatus" ADD VALUE IF NOT EXISTS 'SCHEDULED';
 
